3eceb882e60e98911ca4b3422892b952c061f5f6
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-01-28 19:01:57 +0100

    setting up rabbit mq service

--- checking/libs/common/src/index.ts
+++ checking/libs/common/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./database/absract.repository"
 export * from "./database/database.module"
 export * from "./database/abstract.schema"
+export * from "./rmq/rmq.service"
+export * from "./rmq/rmq.module"
