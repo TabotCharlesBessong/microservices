fdde9a9b2ed15a5fae0d3f91fac830cc199f2709
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-10-11 20:36:03 +0100

    setting basic post service

---
+++ blog/posts/src/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const {randomBytes} = require('crypto');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+
+// Middleware
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const posts = {}
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/posts', (req, res) => {
+  res.send([
+    { id: 1, title: 'First Post', content: 'This is the content of the first post.' },
+    { id: 2, title: 'Second Post', content: 'This is the content of the second post.' },
+  ]);
+});
+
+app.post('/posts', (req, res) => {
+  // Logic to create a new post would go here
+  const id = randomBytes(4).toString('hex');
+  const {title} = req.body;
+
+  posts[id] = { id, title };
+  
+  res.status(201).send({ message: 'Post created' ,posts});
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Posts service running on port ${PORT}`);
+});
