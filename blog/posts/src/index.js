75242d01bb590585c766d68fa667e0f4db8bac6f
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-10-12 21:19:42 +0100

    registering events

--- blog/posts/src/index.js
+++ blog/posts/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {randomBytes} = require('crypto');
 require('dotenv').config();
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 // Middleware
 const cors = require('cors');
@@ -24,17 +25,27 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   // Logic to create a new post would go here
   const id = randomBytes(4).toString('hex');
   const {title} = req.body;
 
   posts[id] = { id, title };
+
+  await axios.post('http://localhost:2376/events', { type: 'PostCreated',
+    data: { id, title }
+  }).catch((err) => {
+    console.log(err.message);
+  });
   
   res.status(201).send({ message: 'Post created' ,posts});
 });
 
+app.post('/events', (req, res) => {
+  console.log('Received Event:', req.body.type);
 
+  res.send({ status: 'OK' });
+});
 
 app.listen(PORT, () => {
   console.log(`Posts service running on port ${PORT}`);
